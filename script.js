f4650c21a855912a7c4a78f18118f51e62f81004
Joyce Huang <65410999+alm0ndjjoy@users.noreply.github.com>
2021-10-06 14:32:18 -0400

    Grabbing and setting a variable for each element

---
+++ script.js
@@ -0,0 +1,5 @@
+// grabbing my elements via HTML
+let messageEl = document.getElementById("message-el");
+let sumEl = document.getElementById("sum-el");
+let cardsEl = document.getElementById("cards-el");
+let newCards = document.getElementById("newCard-btn");
